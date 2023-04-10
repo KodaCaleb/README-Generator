35c1d84ddf7926997a5d35268f01589c6bc9bf94
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-04-10 10:44:35 -0500

    added logic

--- index.js
+++ index.js
@@ -1,12 +1,79 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt ([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Project Name:',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Project Description:',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How to Install?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How to use application?',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'How do people contribute?',
+        },
+        {
+            type: 'input',
+            name: 'testing',
+            message: 'How are you testing project?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose License:',
+            choices: ['MIT', 'GPLv3', 'Apache', 'Unlicense'],
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Enter Github Username:',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter email address:'
+        },
+    ])
+    .then((answers) => {
+        const data = {
+            title: answers.title,
+            description: answers.description,
+            installation: answers.installation,
+            usage: answers.usage,
+            contribution: answers.contribution,
+            testing: answers.testing,
+            license: answers.license,
+            username: answers.username,
+            email: answers.email,
+        };
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    const readmeOutput = generateMarkdown(data);
 
-// TODO: Create a function to initialize app
+
+    fs.writeFile('README.md', readmeOutput, (err) => {
+        if(err) throw err;
+        console.log('README complete!');
+    });
+    })
+    .catch((error) => console.error(error));
+
 function init() {}
 
 // Function call to initialize app
